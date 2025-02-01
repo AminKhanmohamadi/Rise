018da6602d81808b6f2d4980179ed7c5ca8cef08
amin <aminkhm097@gmail.com>
2025-02-01 20:55:14 +0330

    feat: customuser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    TYPE_CHOICES = {
+        'Personal':'personal',
+        'Organization':'organization',
+    }
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='Personal')
+    company_name = models.CharField(max_length=120 , null=True , blank=True)
